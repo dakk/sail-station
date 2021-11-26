21fa138f72a3be8bfd90d62b925050327b120b87
Davide Gessa (dakk) <gessadavide@gmail.com>
2021-11-26 17:44:37 +0100

    stalk_read

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
-sudo wget http://www.stripydog.com/download/kplex_1.3.4-1_armhf.deb
-sudo dpkg -i ./kplex_1.3.4-1_armhf.deb
-sudo systemctl enable kplex
-sudo cp kplex.conf /etc/kplex.conf
-sudo systemctl start kplex
+echo Installing pigpio
+apt-get install pigpio
+systemctl enable pigpiod
+systemctl start pigpiod
+
+echo Installing seatalk2nmea...
+cp stalk_read.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable stalk_read
+systemctl start stalk_read
+
+echo Installing kplex...
+
+wget http://www.stripydog.com/download/kplex_1.3.4-1_armhf.deb
+dpkg -i ./kplex_1.3.4-1_armhf.deb
+systemctl enable kplex
+cp kplex.conf /etc/kplex.conf
+systemctl start kplex
+
+
+echo Creating AP...
+
+
+
+echo Connect to DIVERSA-NMEA wifi network
+echo Done.
