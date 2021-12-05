fcf0b3114b8028feceed67ab9964007cdce481bc
Davide Gessa (dakk) <gessadavide@gmail.com>
2021-12-05 10:58:30 +0100

    fix backup_script

--- backup_sd.sh
+++ backup_sd.sh
@@ -1,2 +1,4 @@
-sudo dd if=/dev/mmcblk0 of=rpi_sail_station.img bs=4M status=progress
-tar -czvf rpi_sail_station.img.tar.gz rpi_sail_station.img
+rm -rf ~/rpi_sail_station.img.tar.gz
+sudo dd if=/dev/mmcblk0 of=~/rpi_sail_station.img bs=32M status=progress
+tar -czvf ~/rpi_sail_station.img.tar.gz ~/rpi_sail_station.img
+rm ~/rpi_sail_station.img
